2abe43d1083b5323fdb812bbd6eacad864c15613
soscolab <61241150+soscolab@users.noreply.github.com>
2021-04-10 11:47:53 +0200

    création fragments

--- app/src/main/java/sc/teach/fragmentsspeciaux/MainActivity.java
+++ app/src/main/java/sc/teach/fragmentsspeciaux/MainActivity.java
@@ -15,19 +15,15 @@ import android.widget.Toast;
 
 import java.util.Calendar;
 
-import sc.teach.fragmentsspeciaux.dialogs.AlertDialogFragment;
-import sc.teach.fragmentsspeciaux.dialogs.DatePickerFragment;
-
+import sc.teach.fragmentsspeciaux.dialogs.PaypalFramgment;
 import sc.teach.fragmentsspeciaux.dialogs.SimpleDialogListener;
 import sc.teach.fragmentsspeciaux.dialogs.SimpleDialogFragment;
 
 
-public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, SimpleDialogListener {
+public class MainActivity extends AppCompatActivity implements  SimpleDialogListener {
 
     Button frag_simple_btn;
-    Button frag_alert_btn;
-    Button frag_date_btn;
-    Button frag_progress_btn;
+    Button frag_paypal_btn;
 
 
     @Override
@@ -35,10 +31,9 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        frag_simple_btn = findViewById(R.id.frag_simple_btn);
-        frag_alert_btn = findViewById(R.id.frag_alert_btn);
-        frag_date_btn = findViewById(R.id.frag_date_btn);
-        frag_progress_btn = findViewById(R.id.frag_progress_btn);
+        frag_simple_btn = findViewById(R.id.PaiementCarte);
+        frag_paypal_btn = findViewById(R.id.PaiementPaypal);
+
 
         frag_simple_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,26 +42,14 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
             }
         });
-        frag_alert_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                showAlertDialog();
 
-            }
-        });
-        frag_date_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) { showDatePickerDialog(frag_date_btn);
-            }
-        });
-        frag_progress_btn.setOnClickListener(new View.OnClickListener() {
+        frag_paypal_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showProgressDialog();
 
             }
         });
-//test
 
     }
 
@@ -79,53 +62,20 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
     }
 
-    private void showAlertDialog() {
 
-        FragmentManager fm = getSupportFragmentManager();
-        AlertDialogFragment alertDialog = AlertDialogFragment.newInstance("Some title");
-        alertDialog.show(fm, "fragment_alert");
 
-    }
 
-    // Afficher le DatePicker
-
-    public void showDatePickerDialog(View v) {
-
-        DatePickerFragment datePickerFragment = new DatePickerFragment();
-        datePickerFragment.show(getSupportFragmentManager(), "datePicker");
-
-    }
-
-    // onDateSet est appellé automatiquement après avoir choisir la date
-    @Override
-    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-
-        // stocker les valeurs sélectionnées dans une instance de type Calendar
-        final Calendar c = Calendar.getInstance();
-        c.set(Calendar.YEAR, year);
-        c.set(Calendar.MONTH, monthOfYear);
-        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-        Toast.makeText(this, c.getTime().toString(), Toast.LENGTH_SHORT).show();
-
-    }
 
     public void showProgressDialog() {
 
-        final ProgressDialog progressDialog = new ProgressDialog(this);
 
-        progressDialog.setTitle("Loading...");
-        progressDialog.setMessage("Please wait.");
-        progressDialog.setCancelable(true);
-        progressDialog.setCancelable(false);
-        progressDialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                progressDialog.dismiss();
+        FragmentManager fm = getSupportFragmentManager();
+        PaypalFramgment fragment_paypal = PaypalFramgment.newInstance("Titre");
+        fragment_paypal.show(fm, "fragment_simple_dialog");
             }
-        });
 
-        progressDialog.show();
-    }
+
+
 
 
     // Cette méthode est appellée dans l'activité quand le listener est déclanché
